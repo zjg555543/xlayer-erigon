64a10fd2ff039a680cf8ec367a93f3ed795de894
jianguo <zjg555543@163.com>
2025-03-12 08:14:48 +0800

    refactor: Rename BuildBlockInfoTreeOld to BuildBlockInfoTreeSerial
    This commit renames the serial block info tree generation function from BuildBlockInfoTreeOld to BuildBlockInfoTreeSerial for improved clarity. The changes include:
    
    - Moved the function from block_info_test.go to block_info.go
    - Updated test cases to use the new function name
    - Removed unused imports in the test file

--- smt/pkg/blockinfo/block_info_test.go
+++ smt/pkg/blockinfo/block_info_test.go
@@ -14,9 +14,7 @@ import (
 	"github.com/ledgerwatch/erigon/crypto"
 	"github.com/ledgerwatch/erigon/smt/pkg/smt"
 	smtutils "github.com/ledgerwatch/erigon/smt/pkg/utils"
-	zktx "github.com/ledgerwatch/erigon/zk/tx"
 	"github.com/ledgerwatch/erigon/zk/utils"
-	"github.com/ledgerwatch/log/v3"
 	"github.com/stretchr/testify/require"
 )
 
@@ -835,7 +833,7 @@ func TestBlockInfoTreeConsistency(t *testing.T) {
 
 			// Execute serial version
 			startSerial := time.Now()
-			serialRoot, err := BuildBlockInfoTreeOld(
+			serialRoot, err := BuildBlockInfoTreeSerial(
 				&coinbase,
 				blockNumber,
 				blockTime,
@@ -889,7 +887,7 @@ func TestBlockInfoTreeEdgeCases(t *testing.T) {
 		)
 		require.NoError(t, err)
 
-		serialRoot, err := BuildBlockInfoTreeOld(
+		serialRoot, err := BuildBlockInfoTreeSerial(
 			&coinbase, blockNumber, blockTime, blockGasLimit, blockGasUsed,
 			ger, l1BlockHash, previousStateRoot, &txInfos,
 		)
@@ -908,7 +906,7 @@ func TestBlockInfoTreeEdgeCases(t *testing.T) {
 		)
 		require.NoError(t, err)
 
-		serialRoot, err := BuildBlockInfoTreeOld(
+		serialRoot, err := BuildBlockInfoTreeSerial(
 			&coinbase, blockNumber, blockTime, blockGasLimit, blockGasUsed,
 			ger, l1BlockHash, previousStateRoot, &txInfos,
 		)
@@ -928,7 +926,7 @@ func TestBlockInfoTreeEdgeCases(t *testing.T) {
 			)
 			require.NoError(t, err)
 
-			serialRoot, err := BuildBlockInfoTreeOld(
+			serialRoot, err := BuildBlockInfoTreeSerial(
 				&coinbase, blockNumber, blockTime, blockGasLimit, blockGasUsed,
 				ger, l1BlockHash, previousStateRoot, &txInfos,
 			)
@@ -974,91 +972,12 @@ func TestBlockInfoTreeConcurrencyStability(t *testing.T) {
 	}
 
 	// Verify consistency with serial version
-	serialRoot, err := BuildBlockInfoTreeOld(
+	serialRoot, err := BuildBlockInfoTreeSerial(
 		&coinbase, blockNumber, blockTime, blockGasLimit, blockGasUsed,
 		ger, l1BlockHash, previousStateRoot, &txInfos,
 	)
 	require.NoError(t, err)
 	require.Equal(t, serialRoot.String(), firstRoot.String())
-}
-
-// BuildBlockInfoTreeOld is a serial implementation of block info tree generation
-// used for testing and comparison with the parallel implementation
-func BuildBlockInfoTreeOld(
-	coinbase *common.Address,
-	blockNumber,
-	blockTime,
-	blockGasLimit,
-	blockGasUsed uint64,
-	ger common.Hash,
-	l1BlockHash common.Hash,
-	previousStateRoot common.Hash,
-	transactionInfos *[]ExecutedTxInfo,
-) (*common.Hash, error) {
-	infoTree := NewBlockInfoTree()
-	keys, vals, err := infoTree.GenerateBlockHeader(&previousStateRoot, coinbase, blockNumber, blockGasLimit, blockTime, &ger, &l1BlockHash)
-	if err != nil {
-		return nil, err
-	}
-
-	log.Trace("info-tree-header",
-		"blockNumber", blockNumber,
-		"previousStateRoot", previousStateRoot.String(),
-		"coinbase", coinbase.String(),
-		"blockGasLimit", blockGasLimit,
-		"blockGasUsed", blockGasUsed,
-		"blockTime", blockTime,
-		"ger", ger.String(),
-		"l1BlockHash", l1BlockHash.String(),
-	)
-	var logIndex int64 = 0
-	for i, txInfo := range *transactionInfos {
-		receipt := txInfo.Receipt
-		t := txInfo.Tx
-
-		l2TxHash, err := zktx.ComputeL2TxHash(
-			t.GetChainID().ToBig(),
-			t.GetValue(),
-			t.GetPrice(),
-			t.GetNonce(),
-			t.GetGas(),
-			t.GetTo(),
-			txInfo.Signer,
-			t.GetData(),
-		)
-		if err != nil {
-			return nil, err
-		}
-
-		log.Trace("info-tree-tx", "block", blockNumber, "idx", i, "hash", l2TxHash.String())
-
-		genKeys, genVals, err := infoTree.GenerateBlockTxKeysVals(&l2TxHash, i, receipt, logIndex, receipt.CumulativeGasUsed, txInfo.EffectiveGasPrice)
-		if err != nil {
-			return nil, err
-		}
-		keys = append(keys, genKeys...)
-		vals = append(vals, genVals...)
-
-		logIndex += int64(len(receipt.Logs))
-	}
-
-	key, val, err := generateBlockGasUsed(blockGasUsed)
-	if err != nil {
-		return nil, err
-	}
-	keys = append(keys, key)
-	vals = append(vals, val)
-
-	insertBatchCfg := smt.NewInsertBatchConfig(context.Background(), "block_info_tree", false)
-	root, err := infoTree.smt.InsertBatch(insertBatchCfg, keys, vals, nil, nil)
-	if err != nil {
-		return nil, err
-	}
-	rootHash := common.BigToHash(root.NewRootScalar.ToBigInt())
-
-	log.Trace("info-tree-root", "block", blockNumber, "root", rootHash.String())
-
-	return &rootHash, nil
 }
 
 // Helper function: Generate test transactions
