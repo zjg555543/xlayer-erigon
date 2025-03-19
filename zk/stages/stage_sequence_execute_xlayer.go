f5f740a2868c34d24fe35909383ac5292154561a
jianguo <zjg555543@163.com>
2025-03-19 16:35:09 +0800

    Refactor SMT alignment functions into stage_sequence_execute_xlayer.go
    - Moved alignExecutionToSMT and resequenceFromSMTAlignment functions from stage_sequence_execute_smt_check_xlayer.go to stage_sequence_execute_xlayer.go.
    - Updated imports to include necessary packages for the moved functions.
    - Enhanced code organization by consolidating SMT-related logic in a single file.

--- zk/stages/stage_sequence_execute_xlayer.go
+++ zk/stages/stage_sequence_execute_xlayer.go
@@ -1,11 +1,14 @@
 package stages
 
 import (
+	"context"
 	"fmt"
 	"time"
 
 	"github.com/0xPolygonHermez/zkevm-data-streamer/datastreamer"
 	dslog "github.com/0xPolygonHermez/zkevm-data-streamer/log"
+	"github.com/ledgerwatch/erigon/core/rawdb"
+	"github.com/ledgerwatch/erigon/eth/stagedsync"
 	"github.com/ledgerwatch/erigon/zk/apollo"
 	"github.com/ledgerwatch/erigon/zk/datastream/server"
 	"github.com/ledgerwatch/log/v3"
@@ -55,4 +58,58 @@ func createExternalDataStreamServer(cfg SequenceBlockCfg) (server.DataStreamServ
 	dataStreamServer := factory.CreateDataStreamServer(streamServer, cfg.zk.L2ChainId)
 
 	return dataStreamServer, nil
+}
+
+func alignExecutionToSMT(batchContext *BatchContext, lastExecutedBlock, smtMaxBlockNumber uint64, u stagedsync.Unwinder) (bool, error) {
+	if lastExecutedBlock > smtMaxBlockNumber {
+		block, err := rawdb.ReadBlockByNumber(batchContext.sdb.tx, smtMaxBlockNumber)
+		if err != nil {
+			return false, err
+		}
+
+		log.Warn(fmt.Sprintf("[%s] Unwinding due to SMT gap", batchContext.s.LogPrefix()), "smtHeight", smtMaxBlockNumber, "sequencerHeight", lastExecutedBlock)
+		u.UnwindTo(smtMaxBlockNumber, stagedsync.BadBlock(block.Hash(), fmt.Errorf("received bad block")))
+		return true, nil
+	}
+	return false, nil
+}
+
+func resequenceFromSMTAlignment(
+	s *stagedsync.StageState,
+	u stagedsync.Unwinder,
+	ctx context.Context,
+	cfg SequenceBlockCfg,
+	historyCfg stagedsync.HistoryCfg,
+	lastBatch, highestBatchInDs uint64,
+) (err error) {
+	log.Info(fmt.Sprintf("[%s] ResequenceFromSMTAlignment, last batch %d is lower than highest batch in datastream %d, resequencing...", s.LogPrefix(), lastBatch, highestBatchInDs))
+
+	batches, err := cfg.dataStreamServer.ReadBatches(lastBatch+1, highestBatchInDs)
+	if err != nil {
+		return err
+	}
+
+	if err = cfg.dataStreamServer.UnwindToBatchStart(lastBatch + 1); err != nil {
+		return err
+	}
+
+	log.Info(fmt.Sprintf("[%s] ResequenceFromSMTAlignment, from batch %d to %d in data stream", s.LogPrefix(), lastBatch+1, highestBatchInDs))
+	for _, batch := range batches {
+		batchJob := NewResequenceBatchJob(batch)
+		subBatchCount := 0
+		for batchJob.HasMoreBlockToProcess() {
+			if err = sequencingBatchStep(s, u, ctx, cfg, historyCfg, batchJob); err != nil {
+				return err
+			}
+
+			subBatchCount += 1
+		}
+
+		log.Info(fmt.Sprintf("[%s] ResequenceFromSMTAlignment, original batch %d with %d batches", s.LogPrefix(), batchJob.batchToProcess[0].BatchNumber, subBatchCount))
+		if cfg.zk.SequencerResequenceStrict && subBatchCount != 1 {
+			return fmt.Errorf("strict mode enabled, but resequenced batch %d has %d sub-batches", batchJob.batchToProcess[0].BatchNumber, subBatchCount)
+		}
+	}
+
+	return nil
 }
